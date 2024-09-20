10fe309e61772074b96d4396af96535f288dfc5a
zxselimcan <zxselimcan@icloud.com>
2024-09-20 17:25:47 +0300

    ssr false

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -76,7 +76,7 @@ export const App: FC = () => {
     appName: 'My RainbowKit App',
     projectId: 'YOUR_PROJECT_ID',
     chains: [mainnet, polygon, optimism, arbitrum, base],
-    ssr: true, // If your dApp uses server side rendering (SSR)
+    ssr: false, // If your dApp uses server side rendering (SSR)
   });
   const queryClient = new QueryClient();
 
