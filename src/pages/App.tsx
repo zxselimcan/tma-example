6a2ec761ce900704c30ee0b277d0d5d247abb221
zxselimcan <zxselimcan@icloud.com>
2024-09-23 12:35:17 +0300

    cdsc

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -45,7 +45,16 @@ const App = () => {
 
 
             customcustom
-            <a href="https://metamask.app.link/dapp/zxselimcan.github.io/tma-example/" target="_blank" rel="noreferrer">Metamask Custom</a>
+            <a href="https://metamask.app.link/dapp/zxselimcan.github.io/tma-example/">Metamask Custom</a>
+
+            <button onClick={() => {
+                // generate and open deep link
+                const url = "https://metamask.app.link/dapp/zxselimcan.github.io/tma-example/";
+                window.open(url, "_blank");
+
+            }}>
+                connect metamask button
+            </button>
         </div>
     )
 }
